1e72526d336a201bde38db06b5ab6fe7c6c64f42
Coel <coeligenai@gmail.com>
2016-05-13 21:17:18 +0300

    no message

--- Casks/dbvisualizer-tgz.rb
+++ Casks/dbvisualizer-tgz.rb
@@ -1,6 +1,6 @@
 cask 'dbvisualizer-tgz' do
-  version '9.2.14'
-  sha256 '91d6793117eecbf51e82527e51208677e0e8f6071f1afdc2b0595674e0eb3ccc'
+  version '9.2.15'
+  sha256 'b3fb9c6729133afffe3f472606d26b745911b2a890172442c416b184dfd746e3'
 
   url "http://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.gsub('.', '_')}.tgz"
   homepage 'http://www.dbvis.com/'
