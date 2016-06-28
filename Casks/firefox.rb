e808552986fcc63b64d61edc3cd0f80ef0821b14
Coel Gena <coeligenai@gmail.com>
2016-06-28 18:44:56 +0300

    no message

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -5,7 +5,7 @@ cask 'firefox' do
   url "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US",
           user_agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36'
   appcast "https://aus5.mozilla.org/update/3/Firefox/#{version}/0/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/Darwin%2015.3.0/default/default/update.xml?force=1",
-          checkpoint: '9cce3ec6844e54c07b7870461fdad111af0c4ce9161dd24c8425885b4f62d372'
+          checkpoint: '81b10965246c46c5a6569f8d777c38b6a5835e02481414a6c951cc39dc533ef7'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/'
   license :mpl
@@ -18,4 +18,10 @@ cask 'firefox' do
                 '~/Library/Application Support/Firefox',
                 '~/Library/Caches/Firefox',
               ]
+
+  caveats <<-EOS.undent
+  The Mac App Store version of 1Password won't work with a Homebrew-cask-linked Mozilla Firefox. To bypass this limitation, you need to either:
+    + Move Mozilla Firefox to your /Applications directory (the app itself, not a symlink).
+    + Install 1Password from outside the Mac App Store (licenses should transfer automatically, but you should contact AgileBits about it).
+  EOS
 end
