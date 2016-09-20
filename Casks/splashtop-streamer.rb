9d224c9be671f2400db47ddff3ed961ccc15df08
Coel <coeligenai@gmail.com>
2016-09-20 16:51:16 +0300

    no message

--- Casks/splashtop-streamer.rb
+++ Casks/splashtop-streamer.rb
@@ -1,6 +1,6 @@
 cask 'splashtop-streamer' do
   version '3.1.0.0'
-  sha256 'c23d0e8b865cf88e2ce39e8be3b1b366883839b280a284a24fae891e3aacaedb'
+  sha256 'ede4ff1ee78ff157fe294e267326d8ac141f272727da523f3deffaf1c104c840'
 
   # d17kmd0va0f0mp.cloudfront.net was verified as official when first introduced to the cask
   url "https://d17kmd0va0f0mp.cloudfront.net/mac/Splashtop_Streamer_Mac_v#{version}.dmg"
