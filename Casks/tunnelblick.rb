494e0b6fa7f2629e3378d5adf900a10e24ce4d2d
Coel <coeligenai@gmail.com>
2016-09-26 19:54:35 +0300

    no message

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -3,8 +3,8 @@ cask 'tunnelblick' do
     version '3.5.9_build_4270.4560'
     sha256 '7651754cab92c5f61fc22b55448875cf14fcf8b6f5b3ba469899740c49b6fae3'
   else
-    version '3.6.8beta06_build_4620'
-    sha256 '541cbb2ee6ebdb2aef7574df8b97aecbcb5de8e23eb85ec53239267d05038978'
+    version '3.6.8beta08_build_4621'
+    sha256 '9b6dc34285d16de9acf18efad6baa7368d0ea07f096f7bb9c3d65f223e1a1e6b'
   end
 
   url "https://www.tunnelblick.net/release/Tunnelblick_#{version}.dmg"
